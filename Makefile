de2fa3d628ba241dbd66aa856d3c01f2f6ac0a0c
darkness <flevn@protonmail.com>
2024-09-12 20:58:35 +0200

    WIP - window and main loop, also reorganised deps

--- Makefile
+++ Makefile
@@ -1,19 +1,23 @@
-CXX        = g++
-OUTPUT_DIR = build
-OUTPUT_BIN = $(OUTPUT_DIR)/stupid_game
-SOURCE     = source/main.C dependencies/glad/src/glad.c
-STD        = -std=gnu++23
-WARNINGS   = -Wall -pedantic
-INC        = -Iinclude -Idependencies/include/SDL2-2.30.7 -Idependencies/include/glm1.0.1 -Idependencies/glad/include/glad
-MISC       = -fno-rtti -fno-exceptions -DGLM_FORCE_SSE2 -DGLM_FORCE_DEFAULT_ALIGNED_GENTYPES
-LINK       = -Ldependencies/libraries/SDL2-2.30.7 -lSDL2 -static
-LINK_DEBUG = -Ldependencies/libraries/SDL2-2.30.7 -lSDL2 -static-libasan
+# I'm so sad I cannot use sanitizers because libraries leak bytes everywhere.
+CXX          = g++
+OUTPUT_DIR   = build
+OUTPUT_BIN   = $(OUTPUT_DIR)/stupid_game
+SOURCE       = source/main.C source/glad.c
+STD          = -std=gnu++23
+WARNINGS     = -Wall -Wpedantic -Wextra -Winline -Wuninitialized -Wshadow -Wconversion -Wstrict-aliasing=2  \
+               -Wnon-virtual-dtor -Wnull-dereference -Woverloaded-virtual -Wformat -Wfloat-equal -Weffc++   \
+               -Wstrict-overflow=5
+INC          = -Iinclude -Idependencies/include
+MISC         = -fno-rtti -fno-exceptions -DGLM_FORCE_SSE2 -DGLM_FORCE_DEFAULT_ALIGNED_GENTYPES
+SDL          = `sdl2-config --cflags --libs`
+LINK_RELEASE = -Wl,--as-needed -Wl,-O1 -Wl,--strip-all -Wl,-Bsymbolic -Wl,--no-undefined -Wl,-z,relro
+LINK_DEBUG   = -Wl,-z,relro
 
 release: build
-	$(CXX) $(STD) -O3   $(WARNINGS) $(INC) $(MISC) $(SOURCE) -o $(OUTPUT_BIN) $(LINK)
+	$(CXX) $(STD) -O3   $(WARNINGS) $(INC) $(MISC) $(SOURCE) -o $(OUTPUT_BIN) $(LINK) $(SDL)
 
 debug: build
-	$(CXX) $(STD) -ggdb -fsanitize=address,undefined $(WARNINGS) $(INC) $(MISC) $(SOURCE) -o $(OUTPUT_BIN) $(LINK_DEBUG)
+	$(CXX) $(STD) -ggdb $(WARNINGS) $(INC) $(MISC) $(SOURCE) -o $(OUTPUT_BIN) $(LINK_DEBUG) $(SDL)
 
 build:
 	mkdir -p $(OUTPUT_DIR)
--- source/main.C
+++ source/main.C
@@ -1,36 +1,73 @@
 #include "SDL.h"
-#include "glm.hpp"
-#include "glad.h"
 #include "depressing.H"
+#include "types.H"
+#include "glad/glad.h"
 
+#include <glm/glm.hpp>
 #include <print>
+#include <memory>
 
 namespace depressing
 {
+  class SDL_subsystem;
+  class window;
+};
+
+// The only global variables that you'll have in this pathetic game. You don't need to worry about
+// the order of initialisation because you need to call their init () functions. Naturally, you first
+// want to initialise SDL's subsystem and then the window.
+std::unique_ptr<depressing::SDL_subsystem> subsystem;
+std::unique_ptr<depressing::window>        game_window;
+b32                                        quit {false};
+
+namespace depressing
+{
+  // Utility class for initialising and destroying SDL window resources.
+  class window
+  {
+  public:
+     window (window const &) = delete;
+     window ();
+    ~window ();
+
+    b32 init ();
+    b32 set_title (char const* title);
+
+    SDL_Window*
+    get_window () const
+    {
+      return _window;
+    }
+
+    window& operator= (window const&) = delete;
+
+  private:
+    SDL_Window* _window;
+    SDL_GLContext _context;
+    i32 _flags;
+    i32 _width;
+    i32 _height;
+    b32 _fullscreen;
+  };
+
   // Utility class for initialising and destroying SDL related resources.
   class SDL_subsystem
   {
   public:
-    SDL_subsystem() = default;
+    SDL_subsystem () = default;
 
     bool
-    init()
+    init ()
     {
-      int constexpr window_flags         = SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_GAMECONTROLLER;
-      int constexpr OpenGL_major_version = 4;
-      int constexpr OpenGL_minor_version = 6;
-      int constexpr OpenGL_double_buffer = 1; // yes
-      int constexpr OpenGL_depth_size    = 24;
+      i32 constexpr flags                {SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_GAMECONTROLLER};
+      i32 constexpr OpenGL_major_version {4};
+      i32 constexpr OpenGL_minor_version {6};
+      i32 constexpr OpenGL_double_buffer {1}; // Means yes, use a double buffer.
+      i32 constexpr OpenGL_depth_size    {24};
 
-      if (SDL_Init(window_flags) < 0)
+      if (SDL_Init (flags) < 0)
 	{
 	  std::print("{0} - Couldn't initialise SDL: {1}\n", __FUNCTION__, SDL_GetError());
-	  return false;
-	}
-
-      if (!gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress))
-	{
-	  std::print("{0} - failed to initialise OpenGL function pointers\n", __FUNCTION__);
 	  return false;
 	}
 
@@ -42,84 +79,137 @@ namespace depressing
       return true;
     }
 
-    ~SDL_subsystem()
+    ~SDL_subsystem ()
     {
-      SDL_Quit();
+      SDL_Quit ();
     }
   };
 
-  class window;
-
-  class window
+  b32
+  window_create ()
   {
-  public:
-     window();
-    ~window();
+    if (!game_window->init ())
+      return false;
 
-    bool init();
-    void set_title(char const* title);
-  private:
-    SDL_Window* _window;
-    SDL_GLContext _context;
-    int _flags;
-    int _width;
-    int _height;
-    bool _fullscreen;
-  };
+    return true;
+  }
+
+  b32
+  window_toggle_fullscreen ()
+  {
+    // TODO: implement
+    return true;
+  }
+
+  b32
+  window_set_title (char const* title)
+  {
+    if (!game_window->set_title (title))
+      return false;
+
+    return true;
+  }
 
   void
-  window_toggle_fullscreen()
+  game_shutdown ()
   {
-    // TODO:
+    quit = true;
+  }
+
+  bool
+  game_is_shutting_down ()
+  {
+    return quit;
+  }
+
+  void
+  game_run ()
+  {
+    while (!game_is_shutting_down ())
+      {
+	SDL_Event event;
+
+	while (SDL_PollEvent (&event))
+	  {
+	    b32 user_forces_shutdown {
+	      event.type == SDL_QUIT ||
+	      (event.type == SDL_WINDOWEVENT &&
+	       event.window.event == SDL_WINDOWEVENT_CLOSE &&
+	       event.window.windowID == SDL_GetWindowID(game_window->get_window()))
+	    };
+
+	    if (user_forces_shutdown)
+	      game_shutdown();
+
+	    // TODO: handle mouse and keyboard SDL events and store them in your structures
+	  }
+
+	if (game_is_shutting_down ())
+	  continue;
+
+	// TEMP
+	glClearColor(0.f, 0.f, 0.f, 1.f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	SDL_GL_SwapWindow(game_window->get_window());
+      }
   }
 };
 
 int
-main()
+main ()
 {
   using namespace depressing;
 
-  SDL_subsystem subsystem;
-  window game_window;
+  subsystem = std::make_unique<SDL_subsystem>();
 
-  if (!subsystem.init())
+  if (!subsystem)
     return EXIT_FAILURE;
 
-  if (!game_window.init())
+  game_window = std::make_unique<window>();
+
+  if (!game_window)
     return EXIT_FAILURE;
+
+  if (!subsystem->init ())
+    return EXIT_FAILURE;
+
+  if (!window_create ())
+    return EXIT_FAILURE;
+
+  game_run ();
 
   return EXIT_SUCCESS;
 }
 
-using namespace depressing;
-
-window::window()
-  : _window{nullptr},
-    _flags{SDL_WINDOW_OPENGL},
-    _width{1920},
-    _height{1080},
-    _fullscreen{false}
+depressing::window::window ()
+  : _window     {nullptr},
+    _context    {nullptr},
+    _flags      {SDL_WINDOW_OPENGL},
+    _width      {640},
+    _height     {480},
+    _fullscreen {false}
 {
 
 }
 
-window::~window()
+depressing::window::~window ()
 {
-  SDL_GL_DeleteContext(_context);
-  SDL_DestroyWindow(_window);
+  SDL_GL_DeleteContext (_context);
+  SDL_DestroyWindow (_window);
 }
 
-bool
-window::init()
+b32
+depressing::window::init ()
 {
   SDL_DisplayMode display_mode;
 
-  SDL_GetDesktopDisplayMode(0, &display_mode);
+  SDL_GetDesktopDisplayMode (0, &display_mode);
 
-  _width = display_mode.w;
+  _width  = display_mode.w;
   _height = display_mode.h;
 
-  _window = SDL_CreateWindow("Depressing Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, _width, _height, _flags);
+  _window = SDL_CreateWindow ("Depressing Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, _width, _height, _flags);
 
   if (!_window)
     {
@@ -127,8 +217,8 @@ window::init()
       return false;
     }
 
-  // XXX: Hack to resize window to native desktop resolution.
-  auto display_index = SDL_GetWindowDisplayIndex(_window);
+  // <XXX: This part is a hack to resize window to native desktop resolution. Figure this crap out.
+  i32 display_index {SDL_GetWindowDisplayIndex (_window)};
 
   if (display_index < 0)
     {
@@ -136,30 +226,43 @@ window::init()
       return false;
     }
 
-  if (SDL_GetDesktopDisplayMode(display_index, &display_mode) != 0)
+  if (SDL_GetDesktopDisplayMode (display_index, &display_mode) != 0)
     {
       std::print("{0} - Couldn't get desktop display mode: %s\n", __FUNCTION__, SDL_GetError());
       return false;
     }
 
-  _width = display_mode.w;
+  _width  = display_mode.w;
   _height = display_mode.h;
 
-  SDL_SetWindowSize(_window, _width, _height);
+  // >XXX
 
-  _context = SDL_GL_CreateContext(_window);
+  SDL_SetWindowSize (_window, _width, _height);
 
-  SDL_GL_MakeCurrent(_window, _context);
+  _context = SDL_GL_CreateContext (_window);
 
-  glEnable(GL_CULL_FACE);
-  glEnable(GL_DEPTH_TEST);
+  SDL_GL_MakeCurrent (_window, _context);
+
+  if (!gladLoadGLLoader ((GLADloadproc)SDL_GL_GetProcAddress))
+    {
+      std::print("{0} - failed to initialise OpenGL function pointers\n", __FUNCTION__);
+      return false;
+    }
+
+
+  glEnable (GL_CULL_FACE);
+  glEnable (GL_DEPTH_TEST);
 
   return true;
 }
 
-
-void
-window::set_title(char const* title)
+b32
+depressing::window::set_title (char const* title)
 {
-  SDL_SetWindowTitle(_window, title);
+  if (!title)
+    return false;
+
+  SDL_SetWindowTitle (_window, title);
+
+  return true;
 }
